3c3924e85bda3e339669834e90bce2b4bc49a179
believeohiozua <believe.ohiozua@gmail.com>
2020-11-19 14:02:00 +0100

    pages 1-3

--- GUI/src/components/containers/Home.js
+++ GUI/src/components/containers/Home.js
@@ -392,34 +392,64 @@ export class Home extends Component {
                         </div>
                     </section>
 
-                    <section id="about" className="about my_font">
+                    <section id="about" className="about my_font" style={{ fontFamily: 'system-ui' }}>
                         <div className="container-fluid border">
-                            <div className="row">
+                            <div className="row py-5">
                                 <div className="col-md-4">
                                     <p className="text-left"><b>Subscribe to our newsletter</b></p>
                                     <form>
                                         <div class="input-group mb-3">
-                                            <div className="">
-                                                <input type="email" class="form-control rounded-pill" placeholder="Recipient's username" />
+                                            <input type="email" class="form-control rounded-pill" placeholder="Your e-mail address" />
+                                            <div class="input-group-append" style={{ position: "relative", left: '-55px', zIndex: 99 }}>
+                                                <span class="input-group-text rounded-pill text-white" style={{ background: 'rgb(1, 27, 51)' }}>Subscribe <i className="la la-angle-right"></i></span>
                                             </div>
-                                            <div class="input-group-append" style={{ zIndex: 99 }}>
-                                                <span class="input-group-text rounded-pill" id="basic-addon2">Subscribe <i className="la la-angle-right"></i></span>
+                                            <div className="row col-12 mt-5">
+                                                <i class="lab la-twitter px-3 h4"></i>
+                                                <i class="lab la-facebook-square px-3 h4"></i>
+                                                <i class="lab la-instagram px-3 h4"></i>
+                                                <i class="lab la-linkedin px-3 h4"></i>
                                             </div>
                                         </div>
                                     </form>
                                 </div>
-                                <div className="col-md-2">
+                                <ul style={{ listStyle: "none" }} className="col-md-2 text-left">
+                                    <p className="text-left"><b>Product</b></p>
+                                    <li>Currency Exchange</li>
+                                    <li> Service Payments</li>
+                                    <li> Bitcoin</li>
+                                    <li> Wallet</li>
+                                    <li> Giftcards</li>
+                                </ul>
 
-                                </div>
-                                <div className="col-md-2">
-
-                                </div>
-                                <div className="col-md-2">
-
-                                </div>
-                                <div className="col-md-2">
-
-                                </div>
+                                <ul style={{ listStyle: "none" }} className="col-md-2 text-left">
+                                    <p className="text-left"><b>Support</b></p>
+                                    <li>Privacy Policy</li>
+                                    <li> Terms of Use</li>
+                                </ul>
+                                <ul style={{ listStyle: "none" }} className="col-md-2 text-left">
+                                    <p className="text-left"><b>Legal</b></p>
+                                    <li>About Us</li>
+                                    <li> Contact </li>
+                                    <li>FAQs</li>
+                                </ul>
+                                <ul style={{ listStyle: "none" }} className="col-md-2 text-left">
+                                    <p className="text-left"><b>Company</b></p>
+                                    <li>Affiliate Program</li>
+                                    <li>  Customer Reviews</li>
+                                </ul>
+                            </div>
+                            <div className="col-10 mx-auto border-top  text-center">
+                                <p className="py-5">
+                                    <small>CoinLynk is a property of Lorem Technologies Limited,
+                                    a company duly registered with CAC with RC Number: 1484208.
+                                    Account numbers and Verve Cards are provided by Rubies Microfinance Bank
+                                    , a company duly registered under the Laws of the Federal Republic of Nigeria;
+                                    and duly licensed by the Central Bank of Nigeria, and pursuant to
+                                       the rules of Verve International Limited.</small>
+                                </p>
+                                <small>
+                                    CoinLynk 2020. All Rights Reserved.
+                                </small>
                             </div>
                         </div>
                     </section>
