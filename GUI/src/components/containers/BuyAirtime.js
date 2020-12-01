58e2bad6da41ca70153d382e9522dab7e8d8c210
believeohiozua <believe.ohiozua@gmail.com>
2020-12-01 15:11:20 +0100

    page 7-9

--- GUI/src/components/containers/BuyAirtime.js
+++ GUI/src/components/containers/BuyAirtime.js
@@ -9,44 +9,71 @@ export class BuyAirtime extends Component {
             <div>
                 <div className="page-wrapper chiller-theme toggled">
                     <SideBar />
-                    <main className="page-content border">
+                    <main class="page-content border">
                         <div className="container-fluid">
                             <div className="row" data-aos="zoom-in" data-aos-delay="100">
                                 <div className="col-md-9">
                                     <div className="row">
 
-                                        <div className="container">
-                                            <h2>Dynamic Tabs with jQuery</h2>
-                                            <p>Click on the Tabs to display the active and previous tab.</p>
-                                            <ul className="nav nav-tabs">
-                                                <li className="active"><a href="#home">Home</a></li>
-                                                <li><a href="#menu1">Menu 1</a></li>
-                                                <li><a href="#menu2">Menu 2</a></li>
-                                                <li><a href="#menu3">Menu 3</a></li>
-                                            </ul>
+                                        <div className="col-md-6 pb-4">
+                                            <div className="col-12 mx-auto border-right border-left" style={{ borderRadius: "1em" }}>
+                                                <ul className="nav nav-tabs row float-center text-center" role="tablist">
+                                                    <li className="nav-item col-6">
+                                                        <a className="nav-link-left active text-muted" data-toggle="tab" href="#panel555" role="tab">
+                                                            Airtime</a>
+                                                    </li>
+                                                    <li className="nav-item col-6">
+                                                        <a className="nav-link-right text-muted" data-toggle="tab" href="#panel666" role="tab">
+                                                            Data
+                                                    </a>
+                                                    </li>
+                                                </ul>
 
-                                            <div className="tab-content">
-                                                <div id="home" className="tab-pane fade in active">
-                                                    <h3>HOME</h3>
-                                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                                                </div>
-                                                <div id="menu1" className="tab-pane fade">
-                                                    <h3>Menu 1</h3>
-                                                    <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                                                </div>
-                                                <div id="menu2" className="tab-pane fade">
-                                                    <h3>Menu 2</h3>
-                                                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
-                                                </div>
-                                                <div id="menu3" className="tab-pane fade">
-                                                    <h3>Menu 3</h3>
-                                                    <p>Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+
+                                                <div className="tab-content">
+                                                    <div className="tab-pane fade in show active off_show" id="panel555" role="tabpanel">
+                                                        <sup className="text-muted " style={{ fontSize: "8px" }}>SELECT WALLET</sup>
+                                                        <div>
+                                                            <div className="form-group">
+                                                                <select
+                                                                    className="digital_asset_1"
+                                                                    searchable="Search here.."
+                                                                    defaultValue={"Select Country"}>
+                                                                    <span className="rounded-circle bg-success text-white p-2">N</span>
+                                                                 &ensp;
+                                                            <span className="font-weight-bold">Naira Wallet</span>
+                                                                    <option disabled>Select Country</option>
+                                                                    <option>Nigeria</option>
+                                                                    <option>Finland</option>
+                                                                    <option>Swiden</option>
+                                                                </select>
+                                                            </div>
+
+                                                        </div>
+                                                    </div>
+
+
+
+                                                    <div className="tab-pane fade" id="panel666" role="tabpanel" style={{ backgroundColor: 'white' }}>
+                                                        <p className="">Buy Section</p>
+
+                                                    </div>
+
+
                                                 </div>
                                             </div>
-                                            <hr />
-                                            <p className="act"><b>Active Tab</b>: <span></span></p>
-                                            <p className="prev"><b>Previous Tab</b>: <span></span></p>
+
                                         </div>
+                                        <div className="col-md-6 text-center mx-auto">
+                                            <div className="border" id="pad_cal_result" style={{ borderRadius: "1em" }}>
+                                                <p className="font-weight-bold text-dark h5 text-center">Total Payout</p>
+                                                <p className="text-dark display-3">NGN 0.00</p>
+                                                <p className="font-weight-bold text-muted">Rate: NGN 0.00</p>
+                                            </div>
+                                        </div>
+
+
+
                                     </div>
 
                                 </div>
